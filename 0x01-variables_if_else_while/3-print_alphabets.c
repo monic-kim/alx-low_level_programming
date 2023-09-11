8b15cbf355890bcf08c6798196a5b93c98556dcc
monic-kim <wambuimonica506@gmail.com>
2023-09-11 06:56:58 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,19 @@
  */
 int main(void)
 {
-	char c;
+	int n = 97;
+	int m = 65;
 
-	c = 'a';
-	while
-		(c <= 'z') {
-			if ((c != 'q' && c != 'e') && c  <= 'z')
-				putchar(c);
-			c++;
-		}
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
 	putchar('\n');
 	return (0);
 }
