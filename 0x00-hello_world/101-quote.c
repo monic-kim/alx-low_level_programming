562554ad9c1034a84183ef3c62917563b9108939
monic-kim <wambuimonica506@gmail.com>
2023-09-11 13:50:18 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n, 59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n",
+	sizeof ("and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19"));
 return (1);
 }
