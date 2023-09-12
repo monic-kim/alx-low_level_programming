cb1d814274817fbc0c37cea04f2812d2c791f0bb
monic-kim <wambuimonica506@gmail.com>
2023-09-12 11:57:05 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,17 @@
 /**
  * main - check the code
  *
+ * print_alphabet Prints the alphabet in lower case
+ *
  * Return: Always 0(success)
  */
 void print_alphabet(void)
 {
-	char c = 'a';
+	int i;
 
-	while (c = 'z')
+	for (i = 97; i <= 122; i++)
 	{
-		_putchar(c);
-		c++;
+		_putchar(i);
 	}
 	_putchar('\n');
 }
