bb4d788dcaa44fc13c8a1d672a8e4c209cfdf242
monic-kim <wambuimonica506@gmail.com>
2023-09-19 11:49:20 -0700

    Winning is only half of it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *  puts_half -  a function that prints half of a string
  *  starting with the first one
@@ -6,7 +7,7 @@
  *  Return:print
  */
 void puts_half(char *str)
-{ 
+{
 	int a, n, longi;
 
 	longi = 0;
@@ -14,15 +15,12 @@ void puts_half(char *str)
 	for (a = 0; str[a] != '\0'; a++)
 		longi++;
 
-	n = (lomgi / 2);
+	n = (longi / 2);
 
 	if ((longi % 2) == 1)
 		n = ((longi + 1) / 2);
 
 	for (a = n; str[a] != '\0'; a++)
-		_putchar(str[a]);
-	_putchar('\n');
+		putchar(str[a]);
+	putchar('\n');
 }
-
-	
-
