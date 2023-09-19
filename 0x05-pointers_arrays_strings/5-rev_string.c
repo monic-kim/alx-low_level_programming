9353d451a94f80b535ed09d89bccc0962b4986d9
monic-kim <wambuimonica506@gmail.com>
2023-09-19 11:58:33 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,14 @@ void rev_string(char *s)
 {
 	char rev = s[0];
 	int counter = 0;
-	inti;
+	int i;
 
 	while (s[counter] != '\0')
 		counter++;
 	for (i = 0; i < counter; i++)
 	{
 		counter--;
-		rev= s[i];
+		rev = s[i];
 		s[i] = s[counter];
 		s[counter] = rev;
 	}
