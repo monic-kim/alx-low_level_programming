05df2669faa13bec012866aaea4393eb23c78dbe
monic-kim <wambuimonica506@gmail.com>
2023-09-21 05:03:28 -0700

    101-print_numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - prints n integer
+ * @n: input integer prameter
+ *
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		putchar(45);
+		i = -1;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	putchar(i % 10 + '0');
+}
