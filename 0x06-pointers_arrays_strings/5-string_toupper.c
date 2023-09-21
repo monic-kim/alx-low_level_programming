fcbbc15786e70b1ec7c9cb6d62a1d61101551fd3
monic-kim <wambuimonica506@gmail.com>
2023-09-21 04:09:05 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper.c - converts a string to upper case
+ * @str: the string input to be converted
+ * Return: modified string
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
