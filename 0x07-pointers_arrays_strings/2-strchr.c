f77004ba37c4709a7cf13d1f2075c448046448f2
monic-kim <wambuimonica506@gmail.com>
2023-09-25 11:33:33 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a charcter in a string
+ * @s: the string to be searched
+ * @c: the character to be located
+ * Return: if c is found - a pointer to the first oc=ucerence.
+ * if c is not found - NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return ('\0');
+}
