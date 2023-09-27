a49e6066c413ca8d4efa29d0cf6d5c00cee5fd2c
monic-kim <wambuimonica506@gmail.com>
2023-09-27 08:31:42 -0700

    prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -11,4 +12,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int sqrt_a(int a, int b);
+int prime_a(int a, int b);
 #endif
