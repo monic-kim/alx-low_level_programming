bd12ac0c4fa83ea4818bb354b861153a7e0e60f4
monic-kim <wambuimonica506@gmail.com>
2023-10-04 13:34:05 -0700

    correctionns

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * Return: Always 0 (success)
 */
 
-int main(int argc__attribute__((unused)); char *argv[])
+int main(int argc, char **argv)
 {
+	(void)argc;
 
 	printf("%s\n", argv[0]);
 	return (0);
