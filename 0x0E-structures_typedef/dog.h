0a52c7a477cbcc2c250c5f9934f2235257607e1d
monic-kim <wambuimonica506@gmail.com>
2023-10-10 12:08:45 -0700

    corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,18 +10,13 @@
  * Desription: first struct with alx
  */
 
-/**
- * dog_t - new name for struct dog
- */
+typedef struct dog
 
-typedef struct dog dog_t;
-
-struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
