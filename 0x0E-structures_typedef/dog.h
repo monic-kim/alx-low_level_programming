db951816d0cf42d9bbc1f4d940b6b43446b2cbfa
monic-kim <wambuimonica506@gmail.com>
2023-10-10 09:45:33 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a struct that reate a dog info
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of thedog
+ *
+ * Desription: first struct with alx
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
