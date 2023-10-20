dadb887f656486a9bfbc36d63783e770108cc733
monic-kim <wambuimonica506@gmail.com>
2023-10-20 15:44:22 -0700

    task

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
+#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: (malloc) strng
@@ -9,9 +11,15 @@
  *
  * Description: singly linked list node structure
  */
-typendef struct list_s
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+#endif
