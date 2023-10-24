7afed957e095cb453d32c1f5a83810e0a80c77c8
monic-kim <wambuimonica506@gmail.com>
2023-10-24 05:07:31 -0700

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all the data (n)
+ * of a listint_t linked list.
+ * @head:double pointer
+ * Return: sum of all the date
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
