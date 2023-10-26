6bb06f16b85ebf90d59c3f571bef4b326073cd72
monic-kim <wambuimonica506@gmail.com>
2023-10-26 06:13:01 -0700

    task 2

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 unsigned long int _pow(unsigned int base, unsigned int power);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
