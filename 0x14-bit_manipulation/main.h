52342365a964e0695d2f24bb2eb8d4e2d45a2dd0
monic-kim <wambuimonica506@gmail.com>
2023-10-26 06:43:08 -0700

    task 4

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ void print_binary(unsigned long int n);
 unsigned long int _pow(unsigned int base, unsigned int power);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
