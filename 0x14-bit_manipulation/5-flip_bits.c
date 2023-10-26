73990d426593baee6cef2dc81404fefdc1f315a8
monic-kim <wambuimonica506@gmail.com>
2023-10-26 06:53:37 -0700

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits -  returns the number of bits you would need
+ * to flip to get from one number to another.
+ * @n:first number
+ * @m: second number
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int x, countbit = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (x = 63; x >= 0; x--)
+	{
+		current = exclusive >> x;
+		if (current & 1)
+			countbit++;
+	}
+	return (countbit);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,5 @@ unsigned long int _pow(unsigned int base, unsigned int power);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif
