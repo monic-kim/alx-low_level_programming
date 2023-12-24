3c2062b2afa866e6f008bcf2e39c16be3f6bb069
monic-kim <wambuimonica506@gmail.com>
2023-12-24 09:59:28 -0800

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,14 +11,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 	unsigned long int n;
 
+	if (size == 0)
+		return (NULL);
+
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->size = size;
+
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
+	{
+		free(ht);
 		return (NULL);
-
+	}
+	ht->size = size;
 	for (n = 0; n < size; n++)
 		ht->array[n] = NULL;
 	return (ht);
